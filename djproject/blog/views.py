256641a7652d65f402284c16fdd53973bea845b5
lohith <viperlohith121@gmail.com>
2019-02-01 21:20:12 -0800

    added some chages

--- djproject/blog/views.py
+++ djproject/blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hey there')
+    latest_question_list = Question.objects.order_by('-pub_date')[:1]
+    output = ', ' .join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)    
+
+def results(request, question_id):
+    response = "You were looking at results of question  %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You were voting on question %s." % question_id)
